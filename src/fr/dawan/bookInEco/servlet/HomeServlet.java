d419efe8e71b4a4a2d57701f805d59bd122d7b2d
Sarah Evain <sarah.evain.se@gmail.com>
2019-11-13 16:53:36 +0100

    Correction du mapping de HomeServlet

--- src/fr/dawan/bookInEco/servlet/HomeServlet.java
+++ src/fr/dawan/bookInEco/servlet/HomeServlet.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class HomeServlet
+ * 
  */
-@WebServlet("/")
+
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,6 +28,8 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//this.getServletContext().getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
+		
 	}
 
 	/**
