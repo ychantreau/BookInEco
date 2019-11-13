dea2c0825deaf1163b75df02fc8c81b82bc88cb1
Yohan Chantreau <ychantreaupro@gmail.com>
2019-11-13 15:16:26 +0100

    Ajout des classes metiers Ajout d'une classe Jdbc pour la connection Ajout d'un main pour tester les classes

---
+++ src/fr/dawan/bookInEco/bean/Commande.java
@@ -0,0 +1,56 @@
+package fr.dawan.bookInEco.bean;
+
+import java.util.List;
+
+public class Commande {
+	private int id;
+	private int idUtilisateur;
+	private List<FicheLivre> listeFicheLivre;
+	private int prixTotalCommande;
+	
+	public void ajouterLivreCommande(FicheLivre livre) {
+		listeFicheLivre.add(livre);
+		prixTotalCommande += livre.getPrix();
+	}
+	
+	
+	public Commande() {
+		super();
+	}
+
+
+	public Commande(int id, int idUtilisateur, List<FicheLivre> listeFicheLivre, int prixTotalCommande) {
+		super();
+		this.id = id;
+		this.idUtilisateur = idUtilisateur;
+		this.listeFicheLivre = listeFicheLivre;
+		this.prixTotalCommande = prixTotalCommande;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+	public List<FicheLivre> getListeFicheLivre() {
+		return listeFicheLivre;
+	}
+	public void setListeFicheLivre(List<FicheLivre> listeFicheLivre) {
+		this.listeFicheLivre = listeFicheLivre;
+	}
+	public int getPrixTotalCommande() {
+		return prixTotalCommande;
+	}
+	public void setPrixTotalCommande(int prixTotalCommande) {
+		this.prixTotalCommande = prixTotalCommande;
+	}
+	
+	
+}
