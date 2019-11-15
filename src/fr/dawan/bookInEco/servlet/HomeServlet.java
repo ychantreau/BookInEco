1e9e5171aacb4f237bd25de29aa8e2bf39320ae0
Abdoulaye Diallo <a.diallo7@hotmail.fr>
2019-11-15 12:48:20 +0100

    Modification HomeServlet et web.xml

--- src/fr/dawan/bookInEco/servlet/HomeServlet.java
+++ src/fr/dawan/bookInEco/servlet/HomeServlet.java
@@ -27,8 +27,8 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		//this.getServletContext().getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request,response);
 		
 	}
 
