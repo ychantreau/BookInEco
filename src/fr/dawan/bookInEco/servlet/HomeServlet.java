ca5a80db1f2d74f74c5a57c5cff15336643d6167
Samson <samson.charlonnai@hotmail.com>
2019-11-21 11:05:48 +0100

    ajout mysql jstl et autres dependencies dans pom.xml - Définition route index dans HomeServlet

--- src/fr/dawan/bookInEco/servlet/HomeServlet.java
+++ src/fr/dawan/bookInEco/servlet/HomeServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HomeServlet
  */
-@WebServlet("/HomeServlet")
+@WebServlet("/")
 public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -27,6 +27,8 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.getRequestDispatcher("WEB-INF/views/").forward(request, response);
 	}
 
 	/**
